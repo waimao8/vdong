3f358c62721b3fe22fbb6bdd341c051778f70ef5
waimao8 <hao999999@gmail.com>
2021-02-22 17:36:15 +0800

    push

--- deploy.sh
+++ deploy.sh
@@ -9,16 +9,34 @@ npm run build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# Set CNAME for "gh-pages" branch
-echo 'ch0769.tk' > CNAME  # 改成你要绑定的域名
-
-msg='deploy'
-githubUrl=git@github.com:ch0769/vdong.git  # 按你的代码仓库信息进行修改
-
+# deploy to github
+#echo 'b.miluluyo.github.io' > CNAME
+if [ -z "$VDOING" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:ch0769/vdong.git
+else
+  msg='来自 github actions的自动部署'
+  githubUrl=https://ch0769:${VDOING}@github.com/ch0769/vdong.git
+  git config --global user.name "ch0769"
+  git config --global user.email "445750168@qq.com"
+fi
 git init
 git add -A
 git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github
+#git push -f git@github.com:ch0769/vdong.git master:gh-pages
+
+
+# deploy to coding
+#echo 'www.miluluu.com\miluluu.com' > CNAME  # 自定义域名
+#if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+#  codingUrl=git@e.coding.net:miluluu/miluluu.git
+#else
+#  codingUrl=https://miluluu:${CODING_TOKEN}@e.coding.net/miluluu/miluluu/miluluu.git
+#fi
+#git add -A
+#git commit -m "${msg}"
+#git push -f $codingUrl master # 推送到coding
 
 cd - # 退回开始所在目录
 rm -rf docs/.vuepress/dist
